2075f7b89f458dbd9fb2f1ff500912c51e01b4ff
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-21 16:56:59 +0300

    changed to arrow functions

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
